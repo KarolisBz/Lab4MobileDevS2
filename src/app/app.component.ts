da9184123c09e5616e58596113dbaa80c3be3b49
Karolis B2 <G00417529@atu.ie>
2024-02-29 15:39:51 +0100

    added all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListcomponentComponent } from './listcomponent/listcomponent.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListcomponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'data-binding-app';
+  isHidden: boolean = true;
+  clickCounter: number = 0;
+
+  btnClicked(): void {
+    this.clickCounter++;
+  }
+
+  btnDoubleClicked(): void {
+    this.isHidden = !this.isHidden;
+  }
 }
